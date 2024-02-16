9594a774f638069d0e6f501340584f4942d9cbe1
Daniel Santos <danielalbertosantosf@gmail.com>
2024-02-16 19:59:47 -0300

    falta juntar arquivos

--- extrair_infodengue.py
+++ extrair_infodengue.py
@@ -3,6 +3,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import os
 import time
 import csv
@@ -29,6 +31,17 @@ def esperar_download(directory, timeout=60):
 
     raise TimeoutError("Timeout waiting for file creation")
 
+def ls_sort_csv(dir):
+    arquivos = os.listdir(dir)
+    arquivos_csv = [arquivo for arquivo in arquivos if arquivo.endswith('.csv')]
+    arquivos_csv.sort(key=lambda x: int(x[-7:-4].strip("()")) if x[-6].isdigit() else 0)
+    return arquivos_csv
+
+def renomear(dir,antigo,novo):
+    dir_ant=os.path.join(dir,antigo)
+    dir_novo=os.path.join(dir,novo)
+    os.rename(dir_ant,dir_novo)
+    
 
 class Extrair_dengue:
     def __init__(self):
@@ -101,8 +114,63 @@ class Extrair_dengue:
                     csv_writer.writerows(csv_reader)
 
                     headers_written = True
-Teste=Extrair_dengue()
+class Extrair_inmet:
+    def __init__(self):
+        self.url_formulario = 'https://tempo.inmet.gov.br/TabelaEstacoes/A001'
+        self.dict_cidade={}
+        self.pasta_dados=os.path.join(os.getcwd(),"data_inmet")
+
+    def inic_driver(self):
+        options=webdriver.ChromeOptions()
+        prefs={"download.default_directory":self.pasta_dados}
+        options.add_experimental_option("detach",True)
+        options.add_experimental_option("prefs",prefs)
+        options.add_argument('--start-minimized')
+        servico= Service(ChromeDriverManager().install())
+        self.driver = webdriver.Chrome(options=options,service=servico)
+        #self.driver.minimize_window()
+        self.driver.get(self.url_formulario)
+        
+    def definir_locais(self):
+            with open(os.path.join(os.getcwd(),"bases\\cods_ride.csv"),'r',newline='') as csvfile:
+                next(csvfile)
+                csv_reader=csv.reader(csvfile,delimiter=';')
+                for linha in csv_reader:
+                    cod_cid=linha[0]
+                    cod_est=linha[4]
+                    self.dict_cidade[cod_cid]=cod_est
+    
+    def get_csv(self,dti,dtf,cod_est):
+        side_but=WebDriverWait(self.driver,10).until(EC.visibility_of_element_located((By.XPATH,'//*[@id="root"]/div[1]/div[1]/i')))
+        side_but.click()
+        estacao=WebDriverWait(self.driver,10).until(EC.visibility_of_element_located((By.XPATH,'//*[@id="root"]/div[2]/div[1]/div[2]/div[3]/input')))
+        data_inic=self.driver.find_element(By.XPATH,'//*[@id="root"]/div[2]/div[1]/div[2]/div[4]/input')
+        data_fim=self.driver.find_element(By.XPATH,'//*[@id="root"]/div[2]/div[1]/div[2]/div[5]/input')
+        gerar_but=self.driver.find_element(By.XPATH,'//*[@id="root"]/div[2]/div[1]/div[2]/button')
+        estacao.send_keys(cod_est)
+        data_inic.send_keys(dti)
+        data_fim.send_keys(dtf)
+        gerar_but.click()
+        download_but=WebDriverWait(self.driver,10).until(EC.visibility_of_element_located((By.XPATH,'//*[@id="root"]/div[2]/div[2]/div/div/div/span/a')))
+        download_but.click()
+    
+    def atualizar_base(self,dti,dtf):
+        self.definir_locais()
+        limpar_pasta(self.pasta_dados)
+        lista_estacs=set(self.dict_cidade.values())
+        ordem=[]
+        for cod in lista_estacs:
+            self.get_csv(dti,dtf,cod)
+            ordem.append(cod)
+        time.sleep(5)
+        self.driver.quit()
+        ordem=[cod+'.csv' for cod in ordem]
+        files_name=ls_sort_csv(self.pasta_dados)
+        for i in range(len(ordem)):
+            renomear(self.pasta_dados,files_name[i],ordem[i])
+
+Teste=Extrair_inmet()
 Teste.inic_driver()
-Teste.puxarinfo()
-Teste.criar_base()
+Teste.atualizar_base('01092023','16022024')
+#Teste.criar_base()
 
